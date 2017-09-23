fe286e97862e54d7e8b3e80655a2abe4be894bcd
DMeiji <doreddomeiji@gmail.com>
2017-09-24 00:55:00 +0900

    一旦コミット

--- material_list_tool/js/PageController.js
+++ material_list_tool/js/PageController.js
@@ -21,54 +21,46 @@
         __name: 'MaterialListTool.controller.PageController',
 
         _logic: MaterialListTool.logic.PageLogic,
-        
-        _getPossessionData: function() {
+
+        // 素材マップ。セット単位で素材合計を出すので
+        // マップの構造は { 0: {xxx: m, yyy: n}, 1: {...} }
+        _materialMap: {},
+
+        _getPossessionData: function () {
             return this.$find('.possessionList').val();
         },
-        
-        _getMaterialData: function() {
+
+        _getMaterialData: function () {
             return this.$find('.materialList').val();
         },
-        
-        _iter: function(data) {
-            // ```
-            // # hoge
-            // ## xxx
-            // - a 5
-            // - b 4
-            // - c 2
-            // ## yyy
-            // - a 3
-            // - d 2
-            // - e 4
-            
-            // # fuga
-            // ## xxx
-            // - a 5
-            // - b 4
-            // - c 2
-            // ## yyy
-            // - a 3
-            // - d 2
-            // - e 4
-            // ```
-            // 解釈の順
-            // 「#」見出しでsplit
-            // 「##」小見出しでsplit
-            // 改行コードでsplit
-            // 「- 」
-            // 「-」子でsplit
-            var headlineList = data.split(/# /);// 見出しごとに分割
-            headLineList.forEach(this.own(function(headLineStr) {
-                var subheadingList = headLineStr.split(/## /);// 小見出しごとに分割
-                subheadingList.forEach(this.own(function() {
-                    var lineList = data.split(/\n/);// 各要素が1行の文字列である配列
-                    lineList.forEach(this.own(function(lineStr, idx){
-        
-                    }));
+
+        _convert: function (data) {
+            var hasConvertErr = false;
+
+            var setBlockStrList = data.split(/# /);// 見出しごとに分割
+            setBlockStrList.forEach(this.own(function (setBlockStr, setIdx) {
+                var lineStrList = setBlockStr.split('\n');// 改行ごとに分割
+                var setName = lineStrList[0].slice(2);// セット名
+
+                lineStrList.forEach(this.own(function (lineStr) {
+                    // 小見出し「## 」は現在出力に使用しないので何もしない
+                    if (/^## /.test(lineStr)) {
+                        return;
+                    }
+                    // 素材行「- 」から素材名と数を取得
+                    var tabIdx = lineStr.match(/\t/).index;
+                    var materialName = lineStr.slice(2, tabIdx);// 素材名。「- 」の後からtabの間の文字
+                    var materialNum = parseInt(lineStr.slice(tabIdx));
+                    hasConvertErr = isNaN(materialNum);// 素材名の数値が文字列から数値に変換できたかチェック
+
+                    // 素材がマップに存在しない場合は0をセット
+                    if (this._materialMap[setIdx][materialName] == nul) {
+                        this._materialMap[materialName] = 0;
+                    }
+                    this._materialMap[materialName] += materialNum;// 素材数を加算
                 }));
             }));
-            
+
         }
     };
 
