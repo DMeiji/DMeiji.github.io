569b3d89cb3af31ee5a886556f1936783191927e
DMeiji <doreddomeiji@gmail.com>
2018-02-22 17:41:17 +0900

    fix xAxis

--- mhw_skill_expectation/js/ExpectationTableContainerController.js
+++ mhw_skill_expectation/js/ExpectationTableContainerController.js
@@ -11,11 +11,28 @@
 
         __name: 'MhwSkillExpectation.controller.ExpectationTableContainerController',
 
+        _expectationData: null,
+
         init: function (expectationData) {
-            this.view.append(this.rootElement, 'ExpectationTableContainer', {
-                expectationData: expectationData,
+            this._expectationData = expectationData;
+        },
+
+        '.showExpectationTableButton click': function (context, $el) {
+            this.view.append('.skillsTableWrapper', 'ExpectationTableContainer', {
+                expectationData: this._expectationData,
                 skillNameMap: skillNameMap
             });
+            this._toggleExpectationTableButton();
+        },
+
+        '.hideExpectationTableButton click': function (context, $el) {
+            this.$find('.skillsTableWrapper').empty();
+            this._toggleExpectationTableButton();
+        },
+
+        _toggleExpectationTableButton: function () {
+            this.$find('.showExpectationTableButton').toggleClass('hide');
+            this.$find('.hideExpectationTableButton').toggleClass('hide');
         }
     };
     h5.core.expose(expectationTableContainerController);
