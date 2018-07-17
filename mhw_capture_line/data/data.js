70886065aabc3260348f3997024dcdfb59467880
DMeiji <doreddomeiji@gmail.com>
2018-07-17 22:24:05 +0900

    ターゲットにバゼル、桜レイア、黒ディアを追加

--- mhw_capture_line/data/data.js
+++ mhw_capture_line/data/data.js
@@ -8,6 +8,21 @@
                 dispName: 'レイギエナ',
                 base: 3000,
                 captureLine: 0.3
+            }, {
+                name: 'bazerugiusu',
+                dispName: 'バゼルギウス',
+                base: 4550,
+                captureLine: 0.3
+            }, {
+                name: 'rioreia-asyu',
+                dispName: 'リオレイア亜種',
+                base: 3000,
+                captureLine: 0.3
+            }, {
+                name: 'deliaburosu-asyu',
+                dispName: 'ディアブロス亜種',
+                base: 3450,
+                captureLine: 0.3
             }
         ]
     });
