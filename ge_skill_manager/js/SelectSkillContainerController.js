894d7a9d37d3cac46e73b250c21976e561b1be16
DMeiji <doreddomeiji@gmail.com>
2018-12-12 18:00:16 +0900

    ローカルにファイルを保存/保存したファイルから復元する機能を追加

--- ge_skill_manager/js/SelectSkillContainerController.js
+++ ge_skill_manager/js/SelectSkillContainerController.js
@@ -52,14 +52,14 @@
          */
         _appendSelectSkillRow: function () {
             // スキル選択行要素をテンプレートから生成して追加
-            var $skillRow = this.view.get('SelectSkillDataRow', {
+            var skillRowHTML = this.view.get('SelectSkillDataRow', {
                 skillDataArray: this._skillDataArray
             });
-            this._$selectSkillList.append($skillRow);
+            this._$selectSkillList.append(skillRowHTML);
 
             // 生成した行にデータアイテムをバインド
             var item = this._createSkillDataItem();
-            this.view.bind($skillRow, {
+            this.view.bind(skillRowHTML, {
                 item: item
             });
             this._skillDataItems.push(item);// データアイテムを配列にキャッシュ
@@ -94,14 +94,18 @@
         '.delete-skill-row-btn click': function (context, $el) {
             var $skillRow = this._getSkillRowElem($el);// 削除対象のスキル行要素
 
-            var rowIdx = this._getSkillRowIndex($skillRow);//　削除対象のスキル行のindex
-            this._skillDataItems.splice(rowIdx, 1);// 削除対象のスキル行にバインドしたデータアイテムを除去
-
-            $skillRow.remove();//　削除太陽のスキル行要素を削除
+            this._deleteSkillRow($skillRow);
 
             this.trigger('updateSkillDataItem', {
                 skillDataItems: this._skillDataItems
             });
+        },
+
+        _deleteSkillRow: function ($skillRow) {
+            var rowIdx = this._getSkillRowIndex($skillRow);//　削除対象のスキル行のindex
+            this._skillDataItems.splice(rowIdx, 1);// 削除対象のスキル行にバインドしたデータアイテムを除去
+
+            $skillRow.remove();//　削除太陽のスキル行要素を削除
         },
 
         /**
@@ -197,6 +201,93 @@
 
             $activeSkillNameCell.empty();
             $activeSkillNameCell.append(resultStr);
+        },
+
+        '.create-file-btn click': function (context, $el) {
+            this.$find('.download-btn').remove();// 既にダウンロードボタンが存在する場合は除去
+
+            var skillDataArray = $.map(this._skillDataItems, function (skillDataItem) {
+                return skillDataItem.get();
+            });
+            var jsonStr = JSON.stringify(skillDataArray);
+            var blob = new Blob([jsonStr], {
+                type: 'text/json'
+            });
+            window.URL = window.URL || window.webkitURL;
+
+            var downloadBtnHTML = '<button class="btn btn-default btn-sm download-btn">';
+            downloadBtnHTML += '<a id="download-link" href="#" target="_blank" download="GE_Skill.json">Download</a>';
+            downloadBtnHTML += '</button>';
+            var $btn = $(downloadBtnHTML);
+            $btn.find('a').attr('href', window.URL.createObjectURL(blob));
+            $el.after($btn);
+        },
+
+        '#download-link click': function (context, $el) {
+            $el.parent('.download-btn').remove();// ダウンロードリンクが押されたら、ダウンロードボタンを除去
+        },
+
+        '.import-btn click': function () {
+            // インポートボタンが押下されたら、非表示にしているinput[type=file]にclickイベントを発火
+            this.$find('.input-file').click();
+        },
+
+        '.input-file change': function (context) {
+            var file = context.event.target.files[0];
+            var reader = new FileReader();
+
+            reader.onload = this.own(function () {
+                var dataArray = JSON.parse(reader.result);
+
+                this._deleteAllSkillRow();// スキル行を全削除
+                $.each(dataArray, this.own(function (idx, data) {
+                    this._appendSelectSkillRow();
+                    var $row = this.$find('.select-skill-data-row').eq(idx);
+                    var item = this._skillDataItems[idx];
+                    var name = data.name;
+                    var melee = data.melee;
+                    var gun = data.gun;
+                    var shield = data.shield;
+                    var enhancePart = data.enhancePart;
+                    var controllerUnit = data.controllerUnit;
+                    var bikou = data.bikou;
+
+                    $row.find('.select-skill').val(name);
+                    $row.find('.input-melee-skill-lv').val(melee);
+                    $row.find('.input-gun-skill-lv').val(gun);
+                    $row.find('.input-shield-skill-lv').val(shield);
+                    $row.find('.input-enhance-part-skill-lv').val(enhancePart);
+                    $row.find('.input-controller-unit-skill-lv').val(controllerUnit);
+                    $row.find('.input-bikou-skill-lv').val(bikou);
+
+                    this._updateActiveSkillBySkillName($row, name);
+
+                    item.set('name', name);
+                    item.set('melee', melee);
+                    item.set('gun', gun);
+                    item.set('shield', shield);
+                    item.set('enhancePart', enhancePart);
+                    item.set('controllerUnit', controllerUnit);
+                    item.set('bikou', bikou);
+                }));
+
+                this.trigger('updateSkillDataItem', {
+                    skillDataItems: this._skillDataItems
+                });
+            });
+
+            reader.readAsText(file);
+        },
+
+        _deleteAllSkillRow: function () {
+            var $skillRows = this.$find('.select-skill-data-row');
+            $.each($skillRows, this.own(function (idx, rowElem) {
+                var $row = $(rowElem);
+                this._deleteSkillRow($row);
+            }));
+            this.trigger('updateSkillDataItem', {
+                skillDataItems: this._skillDataItems
+            });
         }
     };
     h5.core.expose(selectSkillContainerController);
